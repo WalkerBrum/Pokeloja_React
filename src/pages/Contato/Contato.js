adbea59bc4b62aa5684c17a2561c20224363f10a
Walker Lobato <walker.b.lobato@gmail.com>
2022-08-17 17:06:30 -0300

    Implementing the contact page

--- src/pages/Contato/Contato.js
+++ src/pages/Contato/Contato.js
@@ -1,5 +1,64 @@
+import './Contato.scss';
+import photoPerfil from '../../assets/images/perfil-linkedin.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+
 export function Contato() {
     return (
-        <h1>Contato</h1>
+        <div className="contact">
+            <h1>Contato</h1>
+            <div className='profile'>
+                <img src={photoPerfil} alt='Foto de Perfil Walker Lobato'></img>
+                <h3>Walker Lobato</h3>
+                <div className='linkedin'>
+                    <FontAwesomeIcon icon={faLinkedin} />
+                    <span>Linkedin:
+                        <a href='https://www.linkedin.com/in/walkerlobato/' target='_blank' rel='noreferrer'>
+                            https://www.linkedin.com/in/walkerlobato/
+                        </a>
+                    </span>
+                </div>
+                <div className='github'>
+                    <FontAwesomeIcon icon={faGithub} />
+                    <span>Github:
+                        <a href='https://github.com/WalkerBrum' target='_blank' rel='noreferrer'>
+                            https://github.com/WalkerBrum
+                        </a>
+                    </span>
+                </div>
+                <div className='instagram'>
+                    <FontAwesomeIcon icon={faGithub} />
+                    <span>Instagram:
+                        <a href='https://www.instagram.com/brumwalker/' target='_blank' rel='noreferrer'>
+                            https://www.instagram.com/brumwalker/
+                        </a>
+                    </span>
+                </div>
+                <div className='whatsapp'>
+                    <FontAwesomeIcon icon={faWhatsapp} />
+                    <span>Telefone WhatsApp:
+                        <a href='https://web.whatsapp.com/send?phone=55329817-9223' target='_blank' rel='noreferrer'>
+                            (32) 998179223
+                        </a>
+                    </span>
+                </div>
+            </div>
+            <div className='main-skills'>
+                <h3>Sobre</h3>
+                <p>Software Engineer, atualmente trabalhando como freelancer em desenvolvimento de sites e sistemas web para empresas com foco no front-end. Apaixonado por React, utilizo-o na maioria das minhas aplicações, junto com a metodologia ágil Scrum.</p>
+                <h3>Hard Skills</h3>
+                <ul className='hard-skills'>
+                    <li>ReactJs</li>
+                    <li>React Redux</li>
+                    <li>React DOM</li>
+                    <li>Hooks</li>
+                    <li>Axios</li>
+                    <li>CSS</li>
+                    <li>Sass</li>
+                    <li>HTML</li>
+                    <li>Font Awesome</li>
+                </ul>
+            </div>
+        </div>
     )
 }
--- src/pages/Sobre/Sobre.js
+++ src/pages/Sobre/Sobre.js
@@ -4,8 +4,8 @@ import './Sobre.scss';
 export function Sobre() {
     return (
         <div className="about">
+            <h1>Sobre</h1>
             <div className='about-title'>
-                <h1>Sobre</h1>
                 <h3>Nome do projeto: PokéLoja</h3>
                 <p>Aplicação web desenvolvida com ReactJs baseado em um e-commerce de pokémons voltado para a parte de front-end</p>
                 <h3>Tecnologias e bibliotecas ulitizadas</h3>
@@ -23,7 +23,26 @@ export function Sobre() {
             </div>
             <h3>Outros projetos do Software Engineer Walker lobato</h3>
             <div className='other-projects'>
-                <iframe src="https://malanski.github.io/MyResume/" title="My Resume"></iframe>
+                <h4>Buscador de Perfil GitHub: 
+                    <a href='https://github.com/WalkerBrum/search-github-profile-react' alt='Buscador de Perfil GitHub' target='_blank' rel='noreferrer'>Repositório</a>
+                </h4>
+                <iframe src='https://buscador-perfis-github.vercel.app/' title='Buscador de Perfil GitHub'></iframe>
+                <h4>Pomodoro Timer Concentration: 
+                    <a href='https://github.com/WalkerBrum/pomodoro-react' alt='Pomodoro Time' target='_blank' rel='noreferrer'>Repositório</a>
+                </h4>
+                <iframe src='https://pomodoroconcentration.netlify.app/' title='Pomodoro Timer'></iframe>
+                <h4>Sistema de Login/Cadastro: 
+                    <a href='https://github.com/WalkerBrum/login-cadaster-react' alt='Sistema de Login/Cadastro' target='_blank' rel='noreferrer'>Repositório</a>
+                </h4>
+                <iframe src='https://walkerbrum.github.io/login-cadaster-react/' title='Sistema de Login/Cadastro'></iframe>
+                <h4>Jogo da Memória Dragon Ball Z: 
+                    <a href='https://github.com/WalkerBrum/memory-game' alt='Jogo da Memória Dragon Ball Z' target='_blank' rel='noreferrer'>Repositório</a>
+                </h4>
+                <iframe src='https://walkerbrum.github.io/memory-game/' title='Jogo da Memória Dragon Ball Z'></iframe>
+                <h4>To Do List: 
+                    <a href='https://github.com/WalkerBrum/to-do-list' alt='To Do List' target='_blank' rel='noreferrer'>Repositório</a>
+                </h4>
+                <iframe src='https://walkerbrum.github.io/to-do-list/' title='To Do List'></iframe>
             </div>
         </div>
     )
